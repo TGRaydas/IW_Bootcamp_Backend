c2ebaa07673861e28288ec386ad2dd4612a6fc1e
TGRaydas <pgrand@miuandes.cl>
2019-07-04 14:58:05 -0400

    Final advances

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
 	skip_before_action :verify_authenticity_token  
-
+	require 'date'
 	include MainHelper
 	def index
 		#require 'socket'
@@ -11,21 +11,41 @@ class MainController < ApplicationController
 	
 	def value
 		container = Container.find(params[:code])
+		buys = Buy.where(container_id: container.id, )
 		last_buy = container.last_buy
 		last_update = container.updated_at.to_s.split
 		last_update_date = container.updated_at.to_s.split[0]
 		last_update_time = container.updated_at.to_s.split[1]
-		render json: {value: container.value, last_update_time: last_update_time, last_update_date: last_update_date, last_buy: last_buy}
+		now_month = Date.today.month
+		total = 0
+		buys.each do |b|
+			if b.created_at.month > now_month - 1
+				if b.amount != nil
+					total += b.amount
+				end
+			end
+		end
+		render json: {value: container.value, , total: total,last_update_time: last_update_time, last_update_date: last_update_date, last_buy: last_buy}
 	end
 
 	def buy
 		container = Container.find(params[:id])
 		Buy.create(container: container, amount: params[:count])
+		now_month = Date.today.month
+		total = 0
+		buys = Buy.where(container_id: 1)
+		buys.each do |b|
+			if b.created_at.month > now_month - 1
+				if b.amount != nil
+					total += b.amount
+				end
+			end
+		end
 		last_buy = container.last_buy
 		last_update_date = container.updated_at.to_s.split[0]
 		last_update_time = container.updated_at.to_s.split[1]
 		container.update(value: container.value + 1, total: container.total + params[:count].to_i, last_buy: params[:count].to_i)
-		render json: {value: container.value, last_update_time: last_update_time, last_update_date: last_update_date, last_buy: last_buy}
+		render json: {value: container.value, total: total,last_update_time: last_update_time, last_update_date: last_update_date, last_buy: last_buy}
 	end
 
 	def buy_points
